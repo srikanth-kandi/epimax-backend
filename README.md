fe7eddac078f2aa9cb712d15783a978c7de65938
Srikanth Kandi <i.am.srikanth.kandi@gmail.com>
2024-05-01 00:28:04 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,432 @@
+```markdown
+# EpiMax Tasks API
+
+### Database Schema
+
+```sql
+-- PostgreSQL schema creation for users table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+-- PostgreSQL schema creation for tasks table
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    status TEXT NOT NULL,
+    assignee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(assignee_id) REFERENCES users(id)
+);
+```
+
+### Instructions to run the application
+
+1. Clone the repository
+2. Run `npm install` to install the dependencies
+3. Create a `.env` file in the root directory and add the following environment variables
+
+    ```
+    DB_USER=your_db_username
+    DB_PASSWORD=your_db_password
+    DB_NAME=your_db_name
+    DB_HOST=your_db_host
+    JWT_SECRET=your_secret
+    DB_PORT=5432
+    DB_CA=your_db_ca
+    ```
+4. Run `npm start` to start the server
+5. The server will start running on `http://localhost:3000`
+
+### Existing User entries in users table
+
+```json
+{
+    "username": "srikanth",
+    "password": "sri_pass"
+}
+```
+
+```json
+{
+    "username": "testuser",
+    "password": "test@123"
+}
+```
+
+### Existing Tasks entries in tasks table
+
+Tasks created by `srikanth`
+
+```json
+{
+    "title": "do the EpiMax Backend Assignment",
+    "description": "using the ExpressJS and Deploy online",
+    "status": "done"
+}
+```
+
+```json
+{
+    "title": "Resolve pending issues on GitHub",
+    "description": "For te.react.dev and travel-diary-backend",
+    "status": "to do"
+}
+```
+
+Tasks created by `testuser`
+
+```json
+{
+    "title": "test task title",
+    "description": "test task description",
+    "status": "in progress"
+}
+```
+
+### API Endpoints
+
+1. **User Registration**
+
+    - Endpoint: `/register`
+    - Method: `POST`
+    - Request Body:
+        The `"username"` must be unique.
+
+        ```json
+        {
+            "username": "example",
+            "password": "example_password"
+        }
+        ```
+    - Response:
+        - Status Code: `201`
+        - Response Body:
+
+            ```json
+            {
+                "message": "User created successfully"
+            }
+            ```
+    - Error Response:
+        - Status Code: `400`
+        - Response Body:
+        If the `"username"` or `"password"` is missing or `null`.
+
+            ```json
+            {
+                "message": "username and password are required"
+            }
+            ```
+        - Status Code: `400`
+        - Response Body:
+        If the user with the same username already exists.
+
+            ```json
+            {
+                "message": "User with this username already exists"
+            }
+            ```
+        - Status Code: `500`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Internal server error"
+            }
+            ```
+
+2. **User Login**
+
+    - Endpoint: `/login`
+    - Method: `POST`
+    - Request Body:
+        The `"username"` must be registered and the `"password"` must be correct.
+
+        ```json
+        {
+            "username": "example",
+            "password": "example_password"
+        }
+        ```
+    - Response:
+        - Status Code: `200`
+        - Response Body:
+        Returns a token which is required to access the `Tasks` endpoints and is valid for 24 hours.
+
+            ```json
+            {
+                "token": "YOUR_JWT_TOKEN"
+            }
+            ```
+    - Error Response:
+        - Status Code: `400`
+        - Response Body:
+        If the `"username"` or `"password"` is missing or `null`.
+
+            ```json
+            {
+                "message": "username and password are required"
+            }
+            ```
+        - Status Code: `404`
+        - Response Body:
+        If the user with the given username is not found.
+
+            ```json
+            {
+                "message": "User with this username does not exist"
+            }
+            ```
+        - Status Code: `400`
+        - Response Body:
+        If the password is incorrect.
+
+            ```json
+            {
+                "message": "Invalid password"
+            }
+            ```
+        - Status Code: `500`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Internal server error"
+            }
+            ```
+
+3. **Create Task**
+
+    - Endpoint: `/tasks`
+    - Method: `POST`
+    - Request Body:
+
+        ```json
+        {
+            "title": "Task title",
+            "description": "Task description",
+            "status": "to do"
+        }
+        ```
+    - Response:
+        - Status Code: `201`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Task created successfully"
+            }
+            ```
+    - Error Response:
+        - Status Code: `400`
+        - Response Body:
+        If the `"title"`, `"description"`, or `"status"` is missing or `null`.
+
+            ```json
+            {
+                "message": "title, description and status are required"
+            }
+            ```
+        - Status Code: `400`
+        - Response Body:
+        If the status is invalid.
+
+            ```json
+            {
+                "message": "Invalid status, status must be 'to do' or 'in progress' or 'done'"
+            }
+            ```
+        - Status Code: `500`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Internal server error"
+            }
+            ```
+
+4. **Get All Tasks**
+
+    - Endpoint: `/tasks`
+    - Method: `GET`
+    - Response:
+        - Status Code: `200`
+        - Response Body:
+        Returns all tasks created by the logged in user.
+
+            ```json
+            [
+                {
+                    "id": 1,
+                    "title": "Task title",
+                    "description": "Task description",
+                    "status": "to do"
+                },
+                {
+                    "id": 2,
+                    "title": "Another Task",
+                    "description": "Another Task description",
+                    "status": "in progress"
+                }
+            ]
+            ```
+    - Error Response:
+        - Status Code: `404`
+        - Response Body:
+        If there are no tasks created by the user.
+
+            ```json
+            {
+                "message": "No tasks found for this user"
+            }
+            ```
+        - Status Code: `500`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Internal server error"
+            }
+            ```
+
+5. **Get Task by ID**
+
+    - Endpoint: `/tasks/:taskId`
+    - Method: `GET`
+    - Response:
+        - Status Code: `200`
+        - Response Body:
+
+            ```json
+
+
+            {
+                "id": 1,
+                "title": "Task title",
+                "description": "Task description",
+                "status": "to do"
+            }
+            ```
+    - Error Response:
+        - Status Code: `404`
+        - Response Body:
+        If the task with the given ID is not found.
+
+            ```json
+            {
+                "message": "Task not found for this id"
+            }
+            ```
+        - Status Code: `403`
+        - Response Body:
+        If the task does not belong to the logged in user.
+
+            ```json
+            {
+                "message": "You are not authorized to view this task"
+            }
+            ```
+        - Status Code: `500`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Internal server error"
+            }
+            ```
+
+6. **Update Task**
+
+    - Endpoint: `/tasks/:taskId`
+    - Method: `PUT`
+    - Request Body:
+
+        ```json
+        {
+            "title": "Updated Task title",
+            "description": "Updated Task description",
+            "status": "done"
+        }
+        ```
+    - Response:
+        - Status Code: `200`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Task updated successfully"
+            }
+            ```
+    - Error Response:
+        - Status Code: `404`
+        - Response Body:
+        If the task with the given ID is not found.
+
+            ```json
+            {
+                "message": "Task not found for this id"
+            }
+            ```
+        - Status Code: `403`
+        - Response Body:
+        If the task does not belong to the logged in user.
+
+            ```json
+            {
+                "message": "You are not authorized to update this task"
+            }
+            ```
+        - Status Code: `500`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Internal server error"
+            }
+            ```
+
+7. **Delete Task**
+
+    - Endpoint: `/tasks/:taskId`
+    - Method: `DELETE`
+    - Response:
+        - Status Code: `200`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Task deleted successfully"
+            }
+            ```
+    - Error Response:
+        - Status Code: `404`
+        - Response Body:
+        If the task with the given ID is not found.
+
+            ```json
+            {
+                "message": "Task not found for this id"
+            }
+            ```
+        - Status Code: `403`
+        - Response Body:
+        If the task does not belong to the logged in user.
+
+            ```json
+            {
+                "message": "You are not authorized to delete this task"
+            }
+            ```
+        - Status Code: `500`
+        - Response Body:
+
+            ```json
+            {
+                "message": "Internal server error"
+            }
+            ```
---
+++ server.js
@@ -0,0 +1,252 @@
+const express = require("express");
+const { Client } = require("pg");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const dbConfig = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.DB_CA
+  }
+};
+
+const validStatus = ['to do', 'in progress', 'done'];
+
+const client = new Client(dbConfig);
+
+const initializeDBAndServer = async () => {
+  try {
+    await client.connect();
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.error(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+// Endpoint for creating new users
+app.post("/register", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (username === '' || password === '' || !username || !password) {
+            return res.status(400).json({ message: "username and password are required" });
+        }
+        const existingUserQuery = `SELECT * FROM users WHERE username = '${username}';`;
+        const existingUser = await client.query(existingUserQuery);
+        if (existingUser.rows.length > 0) {
+            return res.status(400).json({ message: "User with this username already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const createUserQuery = `
+            INSERT INTO users (username, password_hash)
+            VALUES ('${username}', '${hashedPassword}');
+        `;
+        await client.query(createUserQuery);
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// Endpoint for user login
+app.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (username === '' || password === '' || !username || !password) {
+            return res.status(400).json({ message: "username and password are required" });
+        }
+        const existingUserQuery = `SELECT * FROM users WHERE username = '${username}';`;
+        const existingUser = await client.query(existingUserQuery);
+        if (existingUser.rows.length === 0) {
+            return res.status(400).json({ message: "User with this username does not exist" });
+        }
+        const hashedPassword = existingUser.rows[0].password_hash;
+        const isPasswordCorrect = await bcrypt.compare(password, hashedPassword);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+        const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: "24h" });
+        res.status(200).json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// Middleware to authentication
+const authenticateToken = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const selectUserIdQuery = `SELECT id FROM users WHERE username = '${decodedToken.username}';`;
+        const userId = await client.query(selectUserIdQuery);
+        req.userData = { user_id: userId.rows[0].id };
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ message: "Authentication failed" });
+    }
+};
+
+// Endpoint to create a new task
+app.post("/tasks", authenticateToken, async (req, res) => {
+    try {
+        const { title, description, status } = req.body;
+        if (title === '' || !title || !description || description === '' || !status || status === '') {
+            return res.status(400).json({ message: "title, description and status are required" });
+        }
+        // check if status is valid
+        if (!validStatus.includes(status)) {
+            return res.status(400).json({ message: "Invalid status, status must be 'to do' or 'in progress' or 'done'" });
+        }
+        const createTaskQuery = `
+            INSERT INTO tasks (title, description, status, assignee_id)
+            VALUES ('${title}', '${description}', '${status}', ${req.userData.user_id});
+        `;
+        await client.query(createTaskQuery);
+        res.status(201).json({ message: "Task created successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// Endpoint to get all tasks of logged in user
+app.get("/tasks", authenticateToken, async (req, res) => {
+    try {
+        const getTasksQuery = `SELECT * FROM tasks WHERE assignee_id = ${req.userData.user_id};`;
+        const tasks = await client.query(getTasksQuery);
+        if (tasks.rows.length === 0) {
+            return res.status(404).json({ message: "No tasks found for this user" });
+        } else {
+            const responses = tasks.rows.map(task => {
+                return {
+                    id: task.id,
+                    title: task.title,
+                    description: task.description,
+                    status: task.status
+                };
+            });
+            res.status(200).json(responses);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// Endpoint get a task by id
+app.get("/tasks/:taskId", authenticateToken, async (req, res) => {
+    try {
+        const taskId = req.params.taskId;
+        // check if taskId is a number
+        if (isNaN(taskId) || parseInt(taskId) <= 0) {
+            return res.status(400).json({ message: "Task id must be a positive number" });
+        }
+        const getTaskByIdQuery = `SELECT * FROM tasks WHERE id = ${taskId};`;
+        const taskByIdResponse = await client.query(getTaskByIdQuery);
+        if (taskByIdResponse.rows.length === 0) {
+            return res.status(404).json({ message: "Task not found for this id" });
+        }
+        // check if the task belongs to the logged in user
+        if (taskByIdResponse.rows[0].assignee_id !== req.userData.user_id) {
+            return res.status(403).json({ message: "You are not authorized to view this task" });
+        }
+        const response = {
+            id: taskByIdResponse.rows[0].id,
+            title: taskByIdResponse.rows[0].title,
+            description: taskByIdResponse.rows[0].description,
+            status: taskByIdResponse.rows[0].status
+        };
+        res.status(200).json(response);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// Endpoint to update a task by id
+app.put("/tasks/:taskId", authenticateToken, async (req, res) => {
+    try {
+        const taskId = req.params.taskId;
+        const { title, description, status } = req.body;
+        // check if taskId is a number
+        if (isNaN(taskId) || parseInt(taskId) <= 0) {
+            return res.status(400).json({ message: "Task id must be a positive number" });
+        }
+        // check if status is valid
+        if (status && !validStatus.includes(status)) {
+            return res.status(400).json({ message: "Invalid status, status must be 'to do' or 'in progress' or 'done'" });
+        }
+        const getTaskByIdQuery = `SELECT * FROM tasks WHERE id = ${taskId};`;
+        const taskByIdResponse = await client.query(getTaskByIdQuery);
+        if (taskByIdResponse.rows.length === 0) {
+            return res.status(404).json({ message: "Task not found for this id" });
+        }
+        // check if the task belongs to the logged in user
+        if (taskByIdResponse.rows[0].assignee_id !== req.userData.user_id) {
+            return res.status(403).json({ message: "You are not authorized to update this task" });
+        }
+        // check title and description are not empty
+        const updateFields = [];
+        if (title !== undefined) updateFields.push(`title = '${title}'`);
+        if (description !== undefined) updateFields.push(`description = '${description}'`);
+        if (status !== undefined) updateFields.push(`status = '${status}'`);
+        updateFields.push(`updated_at = CURRENT_TIMESTAMP`);
+
+        if (updateFields.length === 1) {
+            return res.status(400).json({ message: "At least one parameter (title, description, status) must be provided" });
+        }
+
+        const updateTaskQuery = `
+            UPDATE tasks
+            SET ${updateFields.join(', ')}
+            WHERE id = ${taskId};
+        `;
+        await client.query(updateTaskQuery);
+        res.status(200).json({ message: "Task updated successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// Endpoint to delete a task by id
+app.delete("/tasks/:taskId", authenticateToken, async (req, res) => {
+    try {
+        const taskId = req.params.taskId;
+        // check if taskId is a number
+        if (isNaN(taskId) || parseInt(taskId) <= 0) {
+            return res.status(400).json({ message: "Task id must be a positive number" });
+        }
+        const getTaskByIdQuery = `SELECT * FROM tasks WHERE id = ${taskId};`;
+        const taskByIdResponse = await client.query(getTaskByIdQuery);
+        if (taskByIdResponse.rows.length === 0) {
+            return res.status(404).json({ message: "Task not found for this id" });
+        }
+        // check if the task belongs to the logged in user
+        if (taskByIdResponse.rows[0].assignee_id !== req.userData.user_id) {
+            return res.status(403).json({ message: "You are not authorized to delete this task" });
+        }
+        const deleteTaskQuery = `DELETE FROM tasks WHERE id = ${taskId};`;
+        await client.query(deleteTaskQuery);
+        res.status(200).json({ message: "Task deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
